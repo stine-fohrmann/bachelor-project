651dd25bab7e58e75f4468e70f7ff9729b0730a8
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2022-05-01 12:47:48 +0200

    disabled constraint 3

--- integer_linear_optimization.py
+++ integer_linear_optimization.py
@@ -25,11 +25,12 @@ cost = 0
 for i in range(n):
     for j in range(n):
         # print(1.1**cost)
-        f += 1.1**cost * cols[i][j]
+        f += 1.1 ** cost * cols[i][j]
         cost += 1
+        # f += cost * cols[i][j]
 # minimize objective function
-m.Minimize(f)
-#m.Maximize(f)
+# m.Minimize(f)
+m.Maximize(f)
 
 # add constraint equations
 # constraint 1: binary balance
@@ -48,19 +49,19 @@ for i in range(n):
             # uniqueness for rows
             m.Equation(sum(abs(rows[i] - rows[j])) > 1)
 
-'''
 # constraint 3: no triplets
-for i in range(int(n)):
-    # i = 0
-    for j in range(n - 2):
-        # print(j)
-        m.Equation(abs(rows[i][j] + rows[i][j + 1] + rows[i][j + 2] - 3 / 2) == 1 / 2)
-        #m.Equation(abs((rows[i][j] - 1 / 2) + (rows[i][j + 1] - 1 / 2) + (rows[i][j + 2] - 1 / 2)) == 1 / 2)
-        m.Equation(abs(cols[i][j] + cols[i][j + 1] + cols[i][j + 2] - 3 / 2) == 1 / 2)
-        # m.Equation(abs(rows[j][i] + rows[j + 1][i] + rows[j + 2][i] - 3 / 2) == 1 / 2)
+# for i in range(int(3)):
+#     # i = 0
+#     for j in range(n - 2):
+#         # print(j)
+#         # m.Equation(abs(rows[i][j] + rows[i][j + 1] + rows[i][j + 2] - 3 / 2) == 1 / 2)
+#         m.Equation(abs((rows[i][j] - 1 / 2) + (rows[i][j + 1] - 1 / 2) + (rows[i][j + 2] - 1 / 2)) == 1 / 2)
+#         # m.Equation(abs(cols[i][j] + cols[i][j + 1] + cols[i][j + 2] - 3 / 2) == 1 / 2)
+#         m.Equation(abs(rows[j][i] + rows[j + 1][i] + rows[j + 2][i] - 3 / 2) == 1 / 2)
 
-'''
 
+# additional constraint based on objective value
+# m.Equation(f > 140.6)
 
 # method for defining clues / cell values
 def set_cell_value(row_index, col_index, value):
@@ -68,14 +69,14 @@ def set_cell_value(row_index, col_index, value):
 
 
 # define clues / cell values
-#set_cell_value(0, 0, 1)
-#set_cell_value(0, 1, 1)
-#set_cell_value(1, 0, 1)
-#set_cell_value(1, 1, 1)
-#set_cell_value(3, 0, 1)
+# set_cell_value(0, 0, 1)
+# set_cell_value(0, 1, 1)
+# set_cell_value(1, 0, 1)
+# set_cell_value(1, 1, 1)
+# set_cell_value(3, 0, 1)
 
 m.options.SOLVER = 1
-m.solver_options = ['minlp_maximum_iterations 10000', 'minlp_max_iter_with_int_sol 500']
+# m.solver_options = ['minlp_maximum_iterations 10000', 'minlp_max_iter_with_int_sol 500']
 
 try:
     m.solve(disp=True)  # solve
@@ -111,8 +112,15 @@ print(f_value)
 '''
 for i in range(int(n)):
     # i = 0
+    print(f'row {i}')
     for j in range(n - 2):
         # print(j)
         print(abs(rows[i][j].value[0] + rows[i][j + 1].value[0] + rows[i][j + 2].value[0] - 3 / 2))
-    print('next row')
+
+for i in range(int(n)):
+    # i = 0
+    print(f'column {i}')
+    for j in range(n - 2):
+        # print(j)
+        print(abs(cols[i][j].value[0] + cols[i][j + 1].value[0] + cols[i][j + 2].value[0] - 3 / 2))
 '''
