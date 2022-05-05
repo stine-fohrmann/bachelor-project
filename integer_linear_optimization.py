1af03b10ef2d6a658eeec02c4edf021ffd7a4d61
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2022-05-05 10:00:22 +0200

    added 1/fibonacci cost

--- integer_linear_optimization.py
+++ integer_linear_optimization.py
@@ -1,4 +1,3 @@
-import random
 
 from gekko import GEKKO
 import numpy as np
@@ -7,7 +6,7 @@ import numpy as np
 m = GEKKO(remote=True, server='https://byu.apmonitor.com')
 
 # define grid size
-n = 6
+n = 4
 
 # create grid of n*n cells
 cells = np.array([])
@@ -19,20 +18,42 @@ cols = np.array_split(cells, n)
 # print(cols)
 rows = np.transpose(cols)
 
+# generate fibonacci sequence as cost
+fibonacci = np.array([1, 1])
+while len(fibonacci) < n**2:
+    nth = fibonacci[-1] + fibonacci[-2]
+    fibonacci = np.append(fibonacci, nth)
+    fibonacci_split = np.array_split(fibonacci, n)
+# print(fibonacci_split)
+
 # define objective function
 f = 0
-cost = 0
+c = 0
 for i in range(n):
     for j in range(n):
-        # print(1.1**cost)
-        f += 1.1 ** cost * cols[i][j]
-        cost += 1
-        # f += cost * cols[i][j]
+        # cost = c            # simple cost distribution
+        # cost = 1.1**c     # exponential cost
+        cost = 1/fibonacci_split[i][j] # inverse of fibonacci sequence
+        f += cost * cols[i][j]
+        c += 1
 # minimize objective function
 m.Minimize(f)
 # m.Maximize(f)
 
 # add constraint equations
+
+# constraint 3 as conditional statement
+# for j in range(n):
+#     for i in range(n-2):
+#         rows[j][i+2] = m.if3(rows[j][i] + rows[j][i+1]-1, 1, 0 )
+#         cols[j][i + 2] = m.if3(cols[j][i] + cols[j][i + 1] - 1, 1, 0)
+# rows[1][2] = m.if2(rows[1][0] + rows[1][1]-1, 1, 0 )
+# rows[1][3] = m.if2(rows[1][1] + rows[1][2]-1, 1, 0 )
+# rows[1][4] = m.if2(rows[1][2] + rows[1][3]-1, 1, 0 )
+# rows[2][2] = m.if2(rows[2][0] + rows[2][1]-1, 1, 0 )
+# rows[2][3] = m.if2(rows[2][1] + rows[2][2]-1, 1, 0 )
+# rows[2][4] = m.if2(rows[2][2] + rows[2][3]-1, 1, 0 )
+
 # constraint 1: binary balance
 for i in range(n):
     # binary balance for columns
@@ -71,20 +92,42 @@ def set_cell_value(row_index, col_index, value):
 
 
 # define clues / cell values
-# set_cell_value(0, 0, 1)
+# set_cell_value(0, 0, 0)
 # set_cell_value(0, 1, 1)
 # set_cell_value(1, 0, 1)
 # set_cell_value(1, 1, 1)
 # set_cell_value(3, 0, 1)
 
-m.options.SOLVER = 1
+# solver settings
+m.options.SOLVER = 1    # determines which solver to use (APOPT)
 # m.solver_options = ['minlp_maximum_iterations 10000', 'minlp_max_iter_with_int_sol 500']
 
+# method for calculating the value of the objective function after finding a solution
+def calculate_obj_func_val(cols):
+    f_value = 0
+    c = 0
+    for i in range(n):
+        for j in range(n):
+            # cost = c  # simple cost distribution
+            # cost = 1.1**c     # exponential cost
+            cost = 1/fibonacci_split[i][j] # inverse of fibonacci sequence
+            f_value += cost * cols[i][j].value[0]
+            c += 1
+    print(f'calculated value of obj. function: {f_value}')
+
 try:
-    m.solve(disp=True)  # solve
+    m.solve(disp=True)
+
     # print filled out grid
     for row in rows:
         print([int(cell.value[0]) for cell in row])
+
+    # print value of objective function from solver
+    print('Objective: ', -m.options.OBJFCNVAL)
+    # print value of objective function calculated independently
+    calculate_obj_func_val(cols)
+    # calculate_obj_func_val(rows)
+
 except:
     print('Not successful')
     from gekko.apm import get_file
@@ -96,19 +139,10 @@ except:
     with open('infeasibilities.txt', 'w') as fl:
         fl.write(str(f))
 
-print('Objective: ', -m.options.OBJFCNVAL)
 
-# print value of objective function
-'''
-f_value = 0
-cost = 0
-for i in range(n):
-    for j in range(n):
-        # print(1.1**cost)
-        f_value += 1.1**cost * cols[i][j].value[0]
-        cost += 1
-print(f_value)
-'''
+
+
+
 
 # testing if constraint 3 is fulfilled
 '''
