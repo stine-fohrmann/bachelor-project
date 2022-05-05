62f34b38a9560b027df54d2d1d94c9381b696751
stine-fohrmann <61832076+stine-fohrmann@users.noreply.github.com>
2022-05-05 10:47:04 +0200

    added exponential cost

--- plot_obj_func.py
+++ plot_obj_func.py
@@ -7,7 +7,7 @@ Script for plotting objective functions with different cost distributions
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0, 92, 1)
+x = np.arange(0, 100, 1, dtype='int64')
 # generate fibonacci sequence as cost
 fibonacci = np.array([1, 1], dtype='int64')
 while len(fibonacci) < len(x):
@@ -15,8 +15,9 @@ while len(fibonacci) < len(x):
     fibonacci = np.append(fibonacci, nth)
 
 fig1 = plt.figure(1)
-plt.plot(x, fibonacci, label='Fibonacci sequence')
+#plt.plot(x, fibonacci, label='Fibonacci sequence')
 plt.plot(x, x, label='simple cost distribution')
+plt.plot(x, 1.1**x, label='exponential cost')
 plt.title('Different cost distributions')
 plt.legend()
 
